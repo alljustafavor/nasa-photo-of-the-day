cca6779e8b95e9ea5e4c162b6638bc4610a80ee8
Joseph Noblett <noblettjoe@gmail.com>
2023-05-31 19:50:15 -0500

    got basic today's APOD working with basic styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API_KEY, BASE_URL } from "./constants";
+import Header from "./components/Header";
+import TodayAPOD from "./components/TodayAPOD";
+import LastThreeAPOD from "./components/LastThreeAPOD";
+import RandomAPOD from "./components/RandomAPOD";
 import "./App.css";
 
+
 function App() {
+  const [media, setMedia] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        setMedia(res.data);
+      }).then(err => {
+        console.error(err);
+      })
+  })
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
+      <TodayAPOD media_type={media.media_type} url={media.url} title={media.title}/>
+      <LastThreeAPOD />
+      <RandomAPOD />
+      
     </div>
   );
 }
--- src/components/TodayAPOD.js
+++ src/components/TodayAPOD.js
@@ -4,10 +4,13 @@ function TodayAPOD(props) {
 
     return (
         <div className="hero-img-container">
-            <img src={props.thumbnail_url} />
+            {props.media_type
+                ? <iframe height={'800px'} width={'800px'} style={{borderRadius: '50%'}} src={`${props.url}&autoplay=1`} title={props.title} frameborder="0" allowFullScreen />
+                : <img src="" alt="APOD" />}
+            
             <div className="TodayAPOD-info">
-                <h4>Photo of the Day</h4>
-                <p>It is the largest and most complex star forming region in the entire galactic neighborhood. Located in the Large Magellanic Cloud, a small satellite galaxy orbiting our Milky Way galaxy</p>
+                {/* <h4>Photo of the Day</h4>
+                <p>It is the largest and most complex star forming region in the entire galactic neighborhood. Located in the Large Magellanic Cloud, a small satellite galaxy orbiting our Milky Way galaxy</p> */}
             </div>
         </div>
     );
