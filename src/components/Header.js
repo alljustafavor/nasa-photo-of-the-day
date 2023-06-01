30378799169f68200eed62b0cd5d329c9c72fe7f
Joseph Noblett <noblettjoe@gmail.com>
2023-05-31 22:25:48 -0500

    added div to add a circle around header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ function Header() {
     return (
         <div className="header-logo">
             <h1>APOD</h1>
+            <p><small>Astronomy Picture of the Day</small></p>
+            <div className="black-logo-cir"></div>
         </div>
     );
 }
