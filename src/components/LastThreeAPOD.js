a7f895a9bea774bfe4adb9d4a6eccc4325606a60
Joseph Noblett <noblettjoe@gmail.com>
2023-05-31 22:26:11 -0500

    added prop url to img src

--- src/components/LastThreeAPOD.js
+++ src/components/LastThreeAPOD.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-function LastThreeAPOD() {
+function LastThreeAPOD(props) {
 
     return (
         <div className="text">
             <h2>Last Three Picture's of the Day</h2>
             <div className="img-container">
                 <div className="image">
-                    <img src='' />
+                    <img src={props.url} />
                 </div>
                 <div className="image">
-                <img src='' />
+                <img src={props.url} />
                 </div>
                 <div className="image">
-                <img src='' />
+                <img src={props.url} />
                 </div>
             </div>
         </div>    
--- src/components/TodayAPOD.js
+++ src/components/TodayAPOD.js
@@ -4,13 +4,14 @@ function TodayAPOD(props) {
 
     return (
         <div className="hero-img-container">
-            {props.media_type
-                ? <iframe height={'800px'} width={'800px'} style={{borderRadius: '50%'}} src={`${props.url}&autoplay=1`} title={props.title} frameborder="0" allowFullScreen />
-                : <img src="" alt="APOD" />}
+            {props.media_type === 'video'
+                ? <iframe className="content" height={'450px'} width={'450px'} style={{borderRadius: '50%'}} src={`${props.url}?playlist=${props.url}&autoplay=1&controls=1&mute=1&loop=1`} title={props.title} frameborder="0" allowFullScreen />
+                : <img className="content" src="" alt="APOD" />}
             
             <div className="TodayAPOD-info">
-                {/* <h4>Photo of the Day</h4>
-                <p>It is the largest and most complex star forming region in the entire galactic neighborhood. Located in the Large Magellanic Cloud, a small satellite galaxy orbiting our Milky Way galaxy</p> */}
+                <h2>Photo/Video of the Day</h2>
+                <h3>{props.title}</h3>
+                <p>{props.explanation}</p>
             </div>
         </div>
     );
