5f351ce467a3b55e477032bc198a17dd6f47eaa6
Joseph Noblett <noblettjoe@gmail.com>
2023-06-02 08:56:33 -0500

    added src todayAPOD is done

--- src/components/TodayAPOD.js
+++ src/components/TodayAPOD.js
@@ -6,7 +6,7 @@ function TodayAPOD(props) {
         <div className="hero-img-container">
             {props.media_type === 'video'
                 ? <iframe className="content" height={'450px'} width={'450px'} style={{borderRadius: '50%'}} src={`${props.url}?playlist=${props.url}&autoplay=1&controls=1&mute=1&loop=1`} title={props.title} frameborder="0" allowFullScreen />
-                : <img className="content" src="" alt="APOD" />}
+                : <img className="content" src={props.url} alt="APOD" />}
             
             <div className="TodayAPOD-info">
                 <h2>Photo/Video of the Day</h2>
