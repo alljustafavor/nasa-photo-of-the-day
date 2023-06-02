5d3d4471dae7acd464d102267e4ab2bcf8be2add
Joseph Noblett <noblettjoe@gmail.com>
2023-06-02 11:55:41 -0500

    fixed some unused variables

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   const date = new Date();
 
-  let day = date.getDate();
+  // let day = date.getDate();
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
 
