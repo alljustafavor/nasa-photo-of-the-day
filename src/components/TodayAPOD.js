0539394bdf26a09f7a0236883d68dffcd42854c0
Joseph Noblett <noblettjoe@gmail.com>
2023-06-05 14:15:22 -0500

    Added styled to todayAPOD

--- src/components/TodayAPOD.js
+++ src/components/TodayAPOD.js
@@ -1,19 +1,46 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const StyledTodayAPOD = styled.div`
+    background-color: black;
+    color: white;
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: center;
+    padding: 5rem;
+    height: 20vh;
+
+    .TodayAPOD-info {
+        &:hover {
+            transform: scale(1.1);
+            transition: all 0.2s ease-in-out;
+        }
+    }
+
+    img {
+        display: none;
+        &:hover {
+            img {
+                display: flex;
+            }
+            transition: all 0.2s ease-in-out;
+    }
+`
 
 function TodayAPOD(props) {
 
     return (
-        <div className="hero-img-container">
+        <StyledTodayAPOD>
             {props.media_type === 'video'
-                ? <iframe className="content" height={'450px'} width={'450px'} style={{borderRadius: '50%'}} src={`${props.url}?playlist=${props.url}&autoplay=1&controls=1&mute=1&loop=1`} title={props.title} frameborder="0" allowFullScreen />
-                : <img className="content" src={props.url} alt="APOD" />}
+                ? <iframe height={'450px'} width={'450px'} style={{borderRadius: '50%'}} src={`${props.url}?playlist=${props.url}&autoplay=1&controls=1&mute=1&loop=1`} title={props.title} frameborder="0" allowFullScreen />
+                : <img src={props.url} alt="APOD" />}
             
             <div className="TodayAPOD-info">
                 <h2>Photo/Video of the Day</h2>
-                <h3>{props.title}</h3>
-                <p>{props.explanation}</p>
+                <h3>{props.title}Doggo ipsum shoober very good</h3>
+                <p>{props.explanation}Snoot long water shoob aqua doggo adorable doggo, ur givin me a spook long bois. Dat tungg tho bork many pats smol borking doggo with a long snoot for pats he made many woofs heckin good boys, wrinkler he made many</p>
             </div>
-        </div>
+        </StyledTodayAPOD>
     );
 }
 
---
+++ src/theme/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+    black: 'black',
+    white: 'white',
+    breakpointMobile: '(max-width: 550px)',
+  }
